930634d50f3674ed5818bb290380373342781981
Yousab13 <yousabhany2@gmail.com>
2024-11-25 22:12:17 +0200

    search view

--- lib/Features/home/presentation/views/widget/book_details_view_widget.dart
+++ lib/Features/home/presentation/views/widget/book_details_view_widget.dart
@@ -35,4 +35,3 @@ class BookDetailsViewWidget extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/Features/search/presentation/views/search_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/Features/search/presentation/views/widgets/search_view_widget.dart';
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SearchViewWidget();
+  }
+}
